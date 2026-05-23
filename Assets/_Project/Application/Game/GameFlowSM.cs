a3bccc23729c4239c8d875581b3638a3de429fdd
LordPelmeha <sofolov@sfedu.ru>
2026-05-23 21:50:39 +0300

    mostly setup all settings

--- Assets/_Project/Application/Game/GameFlowSM.cs
+++ Assets/_Project/Application/Game/GameFlowSM.cs
@@ -9,5 +9,9 @@ namespace HalfEmpty.Application
 public class GameFlowSM
 {
     public GameStateMachine? StateMachine { get; private set; }
+    /// <summary>Inject a fully constructed machine after state registration.</summary>
+    public void Initialise(GameStateMachine machine) => StateMachine = machine;
+    /// <summary>Convenience: transition the inner state machine.</summary>
+    public void ChangeState(IState newState) => StateMachine?.ChangeState(newState);
 }
 }
